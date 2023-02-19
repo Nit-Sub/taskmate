a203199b6ac2c31812275a9efda2d55d4a110529
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-19 19:40:03 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,18 +13,37 @@ function App() {
       id: 3 , name:"Sub", completed:true
     }
   ])
+  const [show , setShow]=useState(true);
+
+  function handleDelete(id){
+    setTasks(tasks.filter(task=> task.id!==id))
+  
+    
+  }
+  function hide (){
+    setShow(false);
+    console.log(show)
+  }
+  function shows(){
+    setShow(true);
+    console.log(show)
+  }
   return (
+
     <>
     <div className='App'>
       <h1>Task List</h1>
       <ul>
-        {tasks.map((task)=>(
+        {show && tasks.map((task)=>(
           <li key={task.id}>
             <span>{task.id}- {task.name}</span>
-            <button>Delete</button>
+            <button onClick={()=>handleDelete(task.id)}>Delete</button>
           </li>
         ))}
       </ul>
+      <button onClick={()=> shows()} >Show</button>
+      
+      <button onClick={()=> hide()} >hide</button>
 
     </div>
     </>
