bb28e39c294d229494556da24682159d467e02c0
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-19 18:30:17 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [count , setCount]= useState(0);
+  console.log(count)
+  const handleAddition=()=>{
+    setCount(count+1)
+
+  }
+  const handleSubstraction=()=>{
+    setCount(count-1)
+  }
   return (
     <>
-    <Header />
-    <div >
-      App 
+    <div className='App'>
+      <div className='box'>
+    <p>{count}</p>
+    <button onClick={handleAddition}>Add</button>
+    <button onClick={handleSubstraction}>Sub</button>
+      </div>
+
     </div>
-   
-    <Footer/>
     </>
   );
 }
