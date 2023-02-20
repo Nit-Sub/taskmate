f5277ba630d4e65362e3629f4c8790874e2683d6
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-20 16:00:13 +0545

    Update TaskCard.js

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -2,7 +2,7 @@ const TaskCard =({task , handleDelete})=>{
     console.log(task)
     return(
         <>
-        <li key={task.id}>
+        <li >
               <span>{task.id}- {task.name}</span>
               <button onClick={()=>(handleDelete(task.id))}>Delete</button>
             </li>
