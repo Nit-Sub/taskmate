9531d833d39923fdd7b5d2987abf398664b49900
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-20 15:29:43 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,48 +4,48 @@ import Footer from './components/Footer';
 import { useState } from 'react';
 
 function App() {
-  const [tasks , setTasks]= useState([
+  const [tasks, setTasks] = useState([
     {
-      id: 1 , name:"Nitesh", completed:true
-    },{
-      id: 2 , name:"Nit", completed:false
-    },{
-      id: 3 , name:"Sub", completed:true
+      id: 1, name: "Nitesh", completed: true
+    }, {
+      id: 2, name: "Nit", completed: false
+    }, {
+      id: 3, name: "Sub", completed: true
     }
   ])
-  const [show , setShow]=useState(true);
+  const [show, setShow] = useState(true);
 
-  function handleDelete(id){
-    setTasks(tasks.filter(task=> task.id!==id))
-  
-    
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => task.id !== id))
+
+
   }
-  function hide (){
+  function hide() {
     setShow(false);
     console.log(show)
   }
-  function shows(){
+  function shows() {
     setShow(true);
     console.log(show)
   }
   return (
 
     <>
-    <div className='App'>
-      <h1>Task List</h1>
-      <ul>
-        {show && tasks.map((task)=>(
-          <li key={task.id}>
-            <span>{task.id}- {task.name}</span>
-            <button onClick={()=>handleDelete(task.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={()=> shows()} >Show</button>
-      
-      <button onClick={()=> hide()} >hide</button>
+      <div className='App'>
+        <h1>Task List</h1>
+        <ul>
+          {show && tasks.map((task) => (
+            <li key={task.id}>
+              <span>{task.id}- {task.name}</span>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => shows()} >Show</button>
 
-    </div>
+        <button onClick={() => hide()} >hide</button>
+
+      </div>
     </>
   );
 }
