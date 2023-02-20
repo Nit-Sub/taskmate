2e273d3ba619c1509535e9aaf92292a514ed3d7b
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-20 20:05:36 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { useState } from 'react';
 import TaskList from './components/TaskList';
 
 function App() {
