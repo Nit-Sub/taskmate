1325688c1ef4799b76755dffea6174019f6fc65f
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-24 19:50:55 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,35 @@ const [tasks , setTasks] = useState([
   {id:1011, name:'Nites' , completed: false },
   {id:1012, name:'Niteh' , completed: true }
 ])
+const[show , setShow]=useState(false)
+const handleDelete=(id)=>{
+  setTasks(tasks.filter(task=>id !==task.id))
+
+}
+const setting=()=>{
+  setShow(true)
+}
+const hide=()=>{
+  setShow(false)
+}
   return (
     <>
     <div className='main'>
       <div className='body'>
         <h1>Task List</h1>
+        <button onClick={setting}>Show</button>
+        <button onClick={hide}>hide</button>
+        {show &&
         <ul>
-          {
-            tasks.map((task)=>(
-              <li key={task.id}>
-                <span>{task.id}- {task.name}</span>
-                <button>Delete</button>
-              </li>
-            ))
-          }
-        </ul>
+        {
+          tasks.map((task)=>(
+            <li key={task.id}>
+              <span>{task.id}- {task.name}</span>
+              <button onClick={()=>handleDelete(task.id)}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>}
       </div>
     </div>
      
