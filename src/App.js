ca434d5bcf89362937f481d733f0185e1245dbbc
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-24 19:27:40 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
 import './App.css'
 const App = () => {
-const [count , setCount]= useState(0);
-// const add=()=>{
-//   setCount(count+1);
-//     console.log(count)
-// }
-
+const [tasks , setTasks] = useState([
+  {id:1010, name:'Nitesh' , completed: true },
+  {id:1011, name:'Nites' , completed: false },
+  {id:1012, name:'Niteh' , completed: true }
+])
   return (
     <>
     <div className='main'>
       <div className='body'>
-            <p className='p'>{count}</p>
-            <button className='button' onClick={()=>{setCount(count+1)}}> Add </button>
-
-            <button className='button' onClick={()=>{setCount(count-1)}}> Sub</button>
+        <h1>Task List</h1>
+        <ul>
+          {
+            tasks.map((task)=>(
+              <li key={task.id}>
+                <span>{task.id}- {task.name}</span>
+                <button>Delete</button>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     </div>
      
