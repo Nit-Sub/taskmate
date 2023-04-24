15e605474cd7fa2fc04f915454b512b6145ace13
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-24 19:54:28 +0545

    css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import React, { useState } from 'react'
-import './App.css'
-const App = () => {
-const [tasks , setTasks] = useState([
-  {id:1010, name:'Nitesh' , completed: true },
-  {id:1011, name:'Nites' , completed: false },
-  {id:1012, name:'Niteh' , completed: true }
-])
-const[show , setShow]=useState(false)
-const handleDelete=(id)=>{
-  setTasks(tasks.filter(task=>id !==task.id))
+import { useState } from 'react';
+import './App.css';
 
-}
-const setting=()=>{
-  setShow(true)
-}
-const hide=()=>{
-  setShow(false)
-}
+function App() {
+  const [tasks, setTasks] = useState([
+    {id: 5271, name: "Record React Lectures", completed: true}, 
+    {id: 7825, name: "Edit React Lectures", completed: false}, 
+    {id: 8391, name: "Watch Lectures", completed: false}
+  ]);
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id){
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
   return (
-    <>
-    <div className='main'>
-      <div className='body'>
-        <h1>Task List</h1>
-        <button onClick={setting}>Show</button>
-        <button onClick={hide}>hide</button>
-        {show &&
-        <ul>
-        {
-          tasks.map((task)=>(
-            <li key={task.id}>
-              <span>{task.id}- {task.name}</span>
-              <button onClick={()=>handleDelete(task.id)}>Delete</button>
-            </li>
-          ))
-        }
-      </ul>}
-      </div>
+    <div className="App">
+      <h1>Task List</h1>
+      <ul>
+        <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
+        { show && tasks.map((task) => (
+          <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+          </li>
+        )) }
+      </ul>
     </div>
-     
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
