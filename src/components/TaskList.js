4c1d808cbec57e61641cb931eab665c6514b41eb
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-25 11:02:01 +0545

    .

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import { useState } from 'react';
 import TaskCard from './TaskCard';
+import BoxCard from './BoxCard';
 const TaskList = () => {
     const [tasks, setTasks] = useState([
-        {id: 5271, name: "Record React Lectures", completed: true}, 
-        {id: 7825, name: "Edit React Lectures", completed: false}, 
-        {id: 8391, name: "Watch Lectures", completed: false}
-      ]);
-      const [show, setShow] = useState(true);
-    
-      function handleDelete(id){
+        { id: 5271, name: "Record React Lectures", completed: true },
+        { id: 7825, name: "Edit React Lectures", completed: false },
+        { id: 8391, name: "Watch Lectures", completed: false }
+    ]);
+    const [show, setShow] = useState(true);
+
+    function handleDelete(id) {
         setTasks(tasks.filter(task => task.id !== id));
-      }
-  return (
-    <>
-    <h1>Task List</h1>
-      <ul>
-        <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
-        { show && tasks.map((task) => (
-          <TaskCard task={task} handleDelete={handleDelete}/>
-        )) }
-      </ul>
-      <div className="box success">
-        <p className="title">Lorem ipsum dolor sit amet.</p>
-        <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, quo?</p>
-      </div>
-    </>
-  )
+    }
+    return (
+        <>
+            <h1>Task List</h1>
+            <ul>
+                <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
+                {show && tasks.map((task) => (
+                    <TaskCard task={task} handleDelete={handleDelete} />
+                ))}
+            </ul>
+            <BoxCard result="success">
+                <p className="title">Lorem ipsum dolor sit amet.</p>
+                <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, quo?</p>
+            </BoxCard>
+            <BoxCard result="warning">
+                <p className="title">Lorem ipsum dolor sit amet.</p>
+                <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, quo?</p>
+            </BoxCard>
+            <BoxCard result="alert">
+                <p className="title">Lorem ipsum dolor sit amet.</p>
+                <p className="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, quo?</p>
+            </BoxCard>
+        </>
+    )
 }
 
 export default TaskList
