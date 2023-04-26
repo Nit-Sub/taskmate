7fcdd96336affa215118e138fa1f3dcbf77167d2
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-26 19:37:42 +0545

    AddTask

---
+++ src/components/AddTask.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AddTask.css';
+
+const AddTask = () => {
+  return (
+    <>
+    <section className="addtask">
+        <form>
+            <label htmlFor="task">Task Name:</label>
+            <input type="text" name="task" id="task"/>
+        </form>
+    </section>
+    </>
+  )
+}
+
+export default AddTask
