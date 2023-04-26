7bac7f96f82edd1e8e43907cbcafdc81ebcce808
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-26 19:44:19 +0545

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,7 +7,8 @@ const AddTask = () => {
             <section className="addtask">
                 <form>
                     <label htmlFor="task">Task Name:</label>
-                    <input type="text" name="task" id="task" />
+                    <input type="text" name="task" id="task" placeholder='Task Name' autoComplete='off' />
+                    <button type="submit" >AddTask</button>
                 </form>
             </section>
         </>
