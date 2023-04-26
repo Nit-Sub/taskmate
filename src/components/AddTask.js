76919cdd6f29070603ac5ecff96325fa8f4b6c88
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-26 19:37:53 +0545

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './AddTask.css';
 
 const AddTask = () => {
-  return (
-    <>
-    <section className="addtask">
-        <form>
-            <label htmlFor="task">Task Name:</label>
-            <input type="text" name="task" id="task"/>
-        </form>
-    </section>
-    </>
-  )
+    return (
+        <>
+            <section className="addtask">
+                <form>
+                    <label htmlFor="task">Task Name:</label>
+                    <input type="text" name="task" id="task" />
+                </form>
+            </section>
+        </>
+    )
 }
 
 export default AddTask
