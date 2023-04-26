0912959211dd37ca8794dd484bedc2df0c27973d
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-04-26 20:09:35 +0545

    .

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddTask.css';
 
 const AddTask = () => {
+    const [taskValue , setTaskValue]= useState("");
     const handleChange =(event)=>{
-        console.log(event.target.value)
+        setTaskValue(event.target.value)
        
     }
+    const handleReset=()=>{
+        setTaskValue("");
+    }
+    
+    
     return (
         <>
             <section className="addtask">
                 <form>
                     <label htmlFor="task">Task Name:</label>
-                    <input onChange={handleChange} type="text" name="task" id="task" placeholder='Task Name' autoComplete='off' />
+                    <input onChange={handleChange} type="text" name="task" id="task" placeholder='Task Name' autoComplete='off' value={taskValue} />
                     <button type="submit" >AddTask</button>
+                    <span  onClick={handleReset} className='reset'> Reset</span>
+                    
                 </form>
+                <p>{taskValue}</p>
             </section>
         </>
     )
